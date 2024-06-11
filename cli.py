4462fe0a86b28c9cba8da583bdd937077f7ac45f
BlairMutisya <tonnyblair09@gmail.com>
2024-06-11 13:59:13 +0300

    Initialize MovieWatchlistCLI

--- cli.py
+++ cli.py
@@ -3,3 +3,7 @@ from helpers import SessionLocal, Movie, Review, Category
 from models import Base 
 from sqlalchemy.exc import OperationalError
 
+class MovieWatchlistCLI:
+    def __init__(self):
+        self.session = SessionLocal()
+        self.database_initialized = False
