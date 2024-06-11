38c312f7df1532feb7a849d1db57976ba45e24f9
BlairMutisya <tonnyblair09@gmail.com>
2024-06-11 14:02:54 +0300

    Add the Display Main Menu to display the options

--- cli.py
+++ cli.py
@@ -7,3 +7,13 @@ class MovieWatchlistCLI:
     def __init__(self):
         self.session = SessionLocal()
         self.database_initialized = False
+
+    def display_menu(self):
+        """Display the main menu."""
+        click.echo("1. Initialize Database")
+        click.echo("2. Movie Management")
+        click.echo("3. Review Management")
+        click.echo("4. Category Management")
+        click.echo("5. Exit")
+    
+    
