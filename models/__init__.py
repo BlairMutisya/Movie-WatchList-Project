dbe5d8044426ee520a4aa969c38d94172eb90013
uvyne-rop <ropchiri7@gmail.com>
2024-06-11 13:03:16 +0300

    Create SQLAlchemy engine with specified database URL, enabling echo for debugging

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///Movie-Watchlist.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
