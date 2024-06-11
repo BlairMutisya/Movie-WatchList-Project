d392bda34a7cf7fabb72d6db8f80a5af06f2e8c6
Robert Gathanua <robertgathanua@gamail.com>
2024-06-11 15:22:07 +0300

    create Category class

--- helpers.py
+++ helpers.py
@@ -1,3 +1,13 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Float
 from sqlalchemy.orm import relationship, declarative_base
 from models import Base, SessionLocal
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    movies = relationship('Movie', back_populates='category', cascade='all, delete-orphan')
+
+    def _repr_(self):
+        return f"<Category(id={self.id}, name={self.name})>"
